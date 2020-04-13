e484bec721e9a37c1323021be73cd55d952f0a44
ltsimps <ltsimpso@me.com>
2020-04-13 11:38:17 -0400

    can add notes to notes area

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 class App {
   constructor() {
+    this.notes = [];
+
     console.log("Hello World");
     this.$form = document.querySelector("#form");
     this.$noteTitle = document.querySelector("#note-title");
+    this.$noteText = document.querySelector("#note-text");
     this.$formButtons = document.querySelector("#form-buttons");
     this.addEventListeners();
   }
@@ -10,6 +13,17 @@ class App {
   addEventListeners() {
     document.body.addEventListener("click", (event) => {
       this.handleFormClick(event);
+    });
+
+    this.$form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const title = this.$noteTitle.value;
+      const text = this.$noteText.value;
+      const hasNote = title || text;
+      if (hasNote) {
+        // add note
+        this.addNote({ title, text });
+      }
     });
   }
 
@@ -34,6 +48,20 @@ class App {
     this.$noteTitle.style.display = "none";
     this.$formButtons.style.display = "none";
     console.log("closing the form");
+  }
+
+  addNote(note) {
+    const newNote = {
+      title: note.title,
+      text: note.text,
+      color: "white",
+      id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1,
+    };
+
+    this.notes = [...this.notes, newNote];
+    console.log(this.notes);
+    let placeHolder = document.getElementById("placeholder-text");
+    placeHolder.innerText = "new";
   }
 }
 
